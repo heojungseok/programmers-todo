317023a8b2a151af3311c26bcdb60c0df297e2db
heojungseok <tjrwjdgj@gmail.com>
2026-09-19 17:03:18 +0900

    chore: Spring Boot 및 PostgreSQL 개발 환경 초기 설정

---
+++ src/main/java/com/todo/ProgrammersTodoApplication.java
@@ -0,0 +1,13 @@
+package com.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammersTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammersTodoApplication.class, args);
+    }
+
+}

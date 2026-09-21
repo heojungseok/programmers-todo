17775010f70e15ad6187472e321b46d76f4af74b
heojungseok <tjrwjdgj@gmail.com>
2026-09-21 22:55:19 +0900

    test: ApiTodoResponseTest와 중복된 컨트롤러 테스트 정리
    - create, createError, udpate는 ApiTodoResponseTest가 상태 코드와 본문까지 검증하므로 제거

--- src/test/java/com/todo/boundedContext/todo/in/ApiTodoControllerTest.java
+++ src/test/java/com/todo/boundedContext/todo/in/ApiTodoControllerTest.java
@@ -1,15 +1,9 @@
 package com.todo.boundedContext.todo.in;
 
-import com.todo.boundedContext.todo.app.TodoService;
-import com.todo.boundedContext.todo.domain.CompletionStatus;
-import com.todo.boundedContext.todo.dto.TodoCreateRequest;
-import com.todo.boundedContext.todo.dto.TodoResponse;
-import com.todo.boundedContext.todo.dto.TodoUpdateRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.client.RestTestClient;
 
@@ -20,8 +14,6 @@ class ApiTodoControllerTest {
 
     @Autowired
     RestTestClient restClient;
-    @Autowired
-    TodoService todoService;
 
     @Test
     void test1() throws Exception {
@@ -30,32 +22,6 @@ class ApiTodoControllerTest {
                 .exchange()
                 .expectStatus().is2xxSuccessful()
                 .expectBody(String.class).isEqualTo("ok");
-    }
-
-    @Test
-    void create() {
-
-        TodoCreateRequest todoRequest = new TodoCreateRequest("dddd");
-
-        restClient
-                .post().uri("/api/todos")
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(todoRequest)
-                .exchange()
-                .expectStatus().is2xxSuccessful();
-    }
-
-    @Test
-    void createError() {
-
-        TodoCreateRequest todoRequest = new TodoCreateRequest("");
-
-        restClient
-                .post().uri("/api/todos")
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(todoRequest)
-                .exchange()
-                .expectStatus().is4xxClientError();
     }
 
     @Test
@@ -69,18 +35,4 @@ class ApiTodoControllerTest {
                 .jsonPath("$.msg").isEqualTo("존재하지 않는 API 경로입니다.")
                 .jsonPath("$.data").doesNotExist();
     }
-
-    @Test
-    void udpate() {
-
-        TodoResponse response = todoService.create("test todo");
-        TodoUpdateRequest todoRequest = new TodoUpdateRequest(response.getTitle() + " 2", CompletionStatus.Y);
-        restClient
-                .put().uri("/api/todos/" + response.getId())
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(todoRequest)
-                .exchange()
-                .expectStatus().is2xxSuccessful();
-    }
-
 }

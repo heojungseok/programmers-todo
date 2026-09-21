2a61d6a0430b787d0397d70f340c9920b231258a
heojungseok <tjrwjdgj@gmail.com>
2026-09-21 22:59:49 +0900

    refactor: TodoRepository의 중복 선언 제거
    - findAll(Pageable)은 JpaRepository가 이미 제공
    - Spring Data 인터페이스는 @Repository 없이 등록됨

--- src/main/java/com/todo/boundedContext/todo/out/TodoRepository.java
+++ src/main/java/com/todo/boundedContext/todo/out/TodoRepository.java
@@ -5,11 +5,7 @@ import com.todo.boundedContext.todo.domain.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     Page<Todo> findByCompleted(CompletionStatus completed, Pageable pageable);
-
-    Page<Todo> findAll(Pageable pageable);
 }
